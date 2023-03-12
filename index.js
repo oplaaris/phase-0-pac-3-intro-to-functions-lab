7291c6b05c3c209a99f334133c15c5104e082a4f
Victor Leina Ngeny <victorleinangeny@gmail.com>
2023-03-12 16:05:11 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,43 @@
+function shout(string) {
+    return string;
+  }
+  "Hello!".toUpperCase(); 
+  function shout(string) {
+    return string.toUpperCase();
+  }
+
+
+function whisper(string){
+    return string;
+}  
+"HELLO!".toLowerCase(); 
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+
+function logShout(string){
+    console.log();
+}
+"hello!".toUpperCase(); 
+function logShout(string){
+    console.log();
+}
+
+function logWhisper(string){
+    console.log();
+}
+"HELLO".toLowerCase();
+function logWhisper(string){
+    console.log();
+}
+
+function sayHiToHeadphonedRoommate(string){
+    console.log;
+}
+"HELLO".toLowerCase();
+console.log( "I can't hear you!")
+
+function sayHiToHeadphonedRoommate(string){
+    console.log;
+}
